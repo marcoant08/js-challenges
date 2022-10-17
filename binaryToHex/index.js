2eebcdf6ac50ff06fc115187b04b93576dbce632
marcoant08 <marcoant008@gmail.com>
2022-10-17 17:26:17 -0300

    refactor: binaryToHex com comentários

--- binaryToHex/index.js
+++ binaryToHex/index.js
@@ -1,23 +1,31 @@
 "use strict";
 
-// functions
-const separar = (str) => {
-  while (str.length % 4 !== 0) str = `0${str}`;
+// FUNCTIONS
+// separa número binário completo em um array de binários de 4 bits
+const getBinaryArray = (binaryNumber) => {
+  while (binaryNumber.length % 4 !== 0) binaryNumber = `0${binaryNumber}`;
 
-  return Array(str.length / 4)
+  return Array(binaryNumber.length / 4)
     .fill("")
-    .map((_, i) => str.slice(i * 4, i * 4 + 4));
+    .map((_, i) => binaryNumber.slice(i * 4, i * 4 + 4));
 };
 
-// code
-const separado = separar("0111110001");
-console.log("[separado]", separado);
+// converte de binário pra decimal
+const binaryToDecimal = (binaryNumber) => {
+  return binaryNumber.split("").reduce((acc, cur, i) => acc + Number(cur) * Math.pow(2, binaryNumber.length - i - 1), 0);
+};
 
-const dec = separado.map((item) => {
-  return item.split("").reduce((acc, cur, i) => acc + Number(cur) * 2 ** (item.length - i - 1), 0);
-});
-console.log("[dec]", dec);
+// CODE
+const binaryNumber = "100010101010111110101"; // binário a ser convertido
+const binaryArray = getBinaryArray(binaryNumber); // lista de binários de 4 bits
+const decimalList = binaryArray.map((item) => binaryToDecimal(item)); // lista de decimais
+const associations = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]; // 0 a 15 em hexadecimal
+const hexList = decimalList.map((item) => associations[item]); // lista de hexadecimais
+const hexNumber = hexList.join(""); // resultado final (número completo em hexadecimal)
 
-const associations = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
-const hex = dec.map((item) => associations[item]);
-console.log("[hex]", hex.join(""));
+// imprimindo resultados
+console.log("[binaryArray]", binaryArray);
+console.log("[decimalList]", decimalList);
+console.log("[hexList]", hexList);
+console.log("[hexNumber]", hexNumber);
+console.log(`O número ${binaryNumber} em hexadecimal é ${hexNumber}`);
